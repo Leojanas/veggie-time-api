70b42f84f8f764efbaba8649bd661eac741b4aad
Leo Janas <leojanas7@gmail.com>
2021-01-14 11:28:27 -0700

    Complete migrations

---
+++ migrations/004.do.create-garden-table.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS garden;
+
+CREATE TABLE garden (
+    user_id INTEGER REFERENCES users(id) NOT NULL,
+    veggie_id INTEGER REFERENCES veggies(id) NOT NULL,
+    plant_date DATE
+)
